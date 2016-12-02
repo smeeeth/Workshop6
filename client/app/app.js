3bcd9e64d0c288bf80e1f321e6b0666be735c9e0
Eric Smith <ericsmithnh@gmail.com>
2016-12-01 19:02:58 -0500

    failbook

--- client/app/app.js
+++ client/app/app.js
@@ -8,8 +8,8 @@ import ChatPopup from './components/chatpopup';
 import FeedItem from './components/feeditem';
 import {hideElement} from './util';
 import {searchForFeedItems, deleteFeedItem} from './server';
-import { IndexRoute, Router, Route, hashHistory } from 'react-router'
-
+import { IndexRoute, Router, Route, hashHistory } from 'react-router';
+import ErrorBanner from './components/errorbanner';
 /**
  * A fake profile page.
  */
@@ -47,7 +47,7 @@ class SearchResultsPage extends React.Component {
     }
     return searchTerm;
   }
-  
+
   render() {
     var searchTerm = this.getSearchTerm();
     // By using the searchTerm as the key, React will create a new
@@ -67,13 +67,13 @@ class SearchResults extends React.Component {
       results: []
     };
   }
-  
+
   deleteFeedItem(id) {
     deleteFeedItem(id, () => {
       this.refresh();
     });
   }
-  
+
   refresh() {
     var searchTerm = this.props.searchTerm;
     if (searchTerm !== "") {
@@ -90,11 +90,11 @@ class SearchResults extends React.Component {
       });
     }
   }
-  
+
   componentDidMount() {
     this.refresh();
   }
-  
+
   render() {
     return (
       <div>
@@ -134,26 +134,31 @@ class App extends React.Component {
       searchTerm = queryVars.searchTerm;
     }
     return (
-      <div>
-        <NavBar searchTerm={searchTerm} />
-        <div className="container">
-          <div className="row">
-            <div className="col-md-2 fb-left-sidebar">
-              <LeftSideBar />
+        <div>
+          <NavBar searchTerm={searchTerm} />
+          <div className="container">
+            <div className="row">
+            <div className="col-md-12">
+              <ErrorBanner />
             </div>
-            <div className="col-md-7">
-              {this.props.children}
             </div>
-            <div className="col-md-3 fb-right-sidebar">
-              <RightSideBar />
+            <div className="row">
+              <div className="col-md-2 fb-left-sidebar">
+                <LeftSideBar />
+              </div>
+              <div className="col-md-7">
+                {this.props.children}
+              </div>
+              <div className="col-md-3 fb-right-sidebar">
+                <RightSideBar />
+              </div>
             </div>
           </div>
+          <ChatPopup />
         </div>
-        <ChatPopup />
-      </div>
-    )
+      );
+    }
   }
-}
 
 ReactDOM.render((
   <Router history={hashHistory}>
